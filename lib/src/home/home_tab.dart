b50aedd917de7d69aeefc0741c5d06741209c3ba
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-07-12 13:30:44 -0300

    create section category

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
-class HomeTab extends StatelessWidget {
-  const HomeTab({super.key});
+import 'components/category_tile.dart';
+
+class HomeTab extends StatefulWidget {
+  HomeTab({super.key});
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  List<String> categories = [
+    'Frutas',
+    'Grãos',
+    'Verduras',
+    'Temperos',
+    'Cereais',
+  ];
+
+  String selectedCategory = 'Frutas';
 
   @override
   Widget build(BuildContext context) {
@@ -85,12 +102,32 @@ class HomeTab extends StatelessWidget {
               ),
             ),
           ),
+
+          // Categorias
+          Container(
+            padding: const EdgeInsets.only(left: 25),
+            height: 40,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) {
+                return CategoryTile(
+                  category: categories[index],
+                  isSelected: categories[index] == selectedCategory,
+                  onPressed: () {
+                    setState(() {
+                      selectedCategory = categories[index];
+                    });
+                  },
+                );
+              },
+              separatorBuilder: (_, index) => const SizedBox(width: 10),
+              itemCount: categories.length,
+            ),
+          ),
+
+          // Grid
         ],
       ),
-
-      // Categorias
-
-      // Grid
     );
   }
 }
