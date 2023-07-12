074dfba20e97aa76b17e850f2853ff582fa432cf
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-07-12 11:37:02 -0300

    Badge in icon cart and field search

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -31,17 +31,62 @@ class HomeTab extends StatelessWidget {
           ),
         ),
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.shopping_cart,
-              color: CustomColors.customSwatchColor,
+          Padding(
+            padding: const EdgeInsets.only(top: 12, right: 15),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                backgroundColor: CustomColors.customContrastColor,
+                label: const Text(
+                  '2',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+
+      body: Column(
+        children: [
+          // Campo de pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquise aqui...',
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: CustomColors.customContrastColor,
+                  size: 21,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+              ),
             ),
           ),
         ],
       ),
-
-      // Campo de pesquisa
 
       // Categorias
 
